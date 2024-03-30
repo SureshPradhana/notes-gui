b58c2fdc88a5e495c8cf311ac1168d61547ac0a3
SureshPradhana <sureshpradhana19@gmail.com>
2024-03-30 22:33:15 +0530

    dropdown for add

--- src/components/notes/Modal.svelte
+++ src/components/notes/Modal.svelte
@@ -3,9 +3,17 @@
   export let modalAction = null;
   export let handleAction = () => {};
   export let closeModal = () => {};
+  import { notesStore } from "../../stores.js";
 
   let localNote = null;
   let newtag = "";
+  let tagsList = [];
+
+  $: tagsList = [...new Set($notesStore.flatMap((note) => note.tags))];
+
+  $: filteredTags = tagsList.filter((tag) =>
+    tag.toLowerCase().includes(newtag.toLowerCase()),
+  );
 
   function confirmAction() {
     handleAction(localNote);
@@ -26,6 +34,18 @@
       localNote.tags = [...localNote.tags, tag.trim()];
       newtag = "";
     }
+  }
+
+  let selectedSuggestion = "";
+
+  function selectSuggestion(suggestion) {
+    selectedSuggestion = suggestion;
+    newtag = suggestion;
+  }
+
+  function handleInput(event) {
+    selectedSuggestion = "";
+    newtag = event.target.value;
   }
 </script>
 
@@ -50,23 +70,29 @@
           <p>No tags</p>
         {/if}
       </div>
+
       <div class="add-tag-wrapper">
-        <input type="text" placeholder="Add tag" bind:value={newtag} />
+        <div>
+          <input type="text" bind:value={newtag} on:input={handleInput} />
+          <div class="dropdown">
+            {#if newtag}
+              <ul>
+                {#each filteredTags as tag}
+                  <li on:click={() => selectSuggestion(tag)}>
+                    {tag}
+                  </li>
+                {/each}
+              </ul>
+            {/if}
+          </div>
+        </div>
+
         <button on:click={() => addTag(newtag)}>Add tag</button>
       </div>
+
       <div class="button-wrapper">
         <button on:click={closeModal}>Cancel</button>
-        <!-- Call closeModal when clicked -->
-        <button class="done" on:click={confirmAction}>Add</button>
-      </div>
-    {:else if modalAction === "delete"}
-      <div class="info">
-        <h2>Are you sure you want to {modalAction} this note?</h2>
-      </div>
-      <div class="button-wrapper">
-        <button on:click={closeModal}>No</button>
-        <!-- Call closeModal when clicked -->
-        <button class="done" on:click={confirmAction}>Yes</button>
+        <button class="done" on:click={confirmAction}>Save</button>
       </div>
     {:else if modalAction === "edit"}
       <h2>Edit note</h2>
@@ -102,4 +128,46 @@
 
 <style lang="scss">
   @import "../styles/model.scss";
+  .add-tag-wrapper input {
+    width: 100%;
+  }
+  .add-tag-wrapper {
+    position: relative;
+  }
+  .add-tag-wrapper div {
+    position: relative;
+  }
+
+  .add-tag-wrapper div .dropdown {
+    width: -webkit-fill-available;
+    position: absolute;
+    left: 0;
+    background: var(--black-2);
+    color: white;
+    height: min-content;
+    max-height: 200px;
+    overflow: scroll;
+  }
+  .dropdown {
+    ul {
+      list-style: none;
+      padding: 0;
+      margin: 0;
+      border: 1px solid #ccc; /* Border style */
+      max-height: 200px; /* Optional: Limit maximum height */
+      overflow-y: auto; /* Optional: Add scrollbar if content exceeds height */
+    }
+
+    /* Style for <li> elements inside the <ul> */
+    ul li {
+      padding: 10px;
+      cursor: pointer;
+      border-bottom: 1px solid #ccc; /* Optional: Add bottom border between list items */
+    }
+
+    /* Style for the last <li> to remove bottom border */
+    ul li:last-child {
+      border-bottom: none; /* Remove bottom border for the last item */
+    }
+  }
 </style>
--- src/utils/db.js
+++ src/utils/db.js
@@ -5,186 +5,186 @@ var path = "https://notes-api-3xdk.onrender.com";
 // var path = "http://localhost:3000";
 
 export const deleteNote = (id) => {
-	var tokenValue = get(token);
+  var tokenValue = get(token);
 
-	var res = fetch(`${path}/api/byid/`, {
-		method: "DELETE",
-		headers: {
-			"Content-Type": "application/json",
-			"Authorization": tokenValue,
-		},
-		body: JSON.stringify({ _id: id }),
-	}).then((response) => response.json()).then((data) => {
-		return data;
-	});
-	return res;
+  var res = fetch(`${path}/api/byid/`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": tokenValue,
+    },
+    body: JSON.stringify({ _id: id }),
+  }).then((response) => response.json()).then((data) => {
+    return data;
+  });
+  return res;
 };
 
 export const deleteBucket = (id) => {
-	var tokenValue = get(token);
+  var tokenValue = get(token);
 
-	var res = fetch(`${path}/api/bucketlist/bybucketId`, {
-		method: "DELETE",
-		headers: {
-			"Content-Type": "application/json",
-			"Authorization": tokenValue,
-		},
-		body: JSON.stringify({ _id: id }),
-	}).then((response) => response.json()).then((data) => {
-		return data;
-	});
-	return res;
+  var res = fetch(`${path}/api/bucketlist/bybucketId`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": tokenValue,
+    },
+    body: JSON.stringify({ _id: id }),
+  }).then((response) => response.json()).then((data) => {
+    return data;
+  });
+  return res;
 };
 export const deleteCard = (id) => {
-	var tokenValue = get(token);
+  var tokenValue = get(token);
 
-	var res = fetch(`${path}/api/flashcards/bycardId`, {
-		method: "DELETE",
-		headers: {
-			"Content-Type": "application/json",
-			"Authorization": tokenValue,
-		},
-		body: JSON.stringify({ _id: id }),
-	}).then((response) => response.json()).then((data) => {
-		return data;
-	});
-	return res;
+  var res = fetch(`${path}/api/flashcards/bycardId`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": tokenValue,
+    },
+    body: JSON.stringify({ _id: id }),
+  }).then((response) => response.json()).then((data) => {
+    return data;
+  });
+  return res;
 };
 
 export const addNote = (note) => {
-	var tokenValue = get(token);
+  var tokenValue = get(token);
 
-	var res = fetch(`${path}/api/note/`, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-			"Authorization": tokenValue,
-		},
-		body: JSON.stringify(note),
-	}).then((response) => response.json()).then((data) => {
-		return data;
-	});
-	return res;
+  var res = fetch(`${path}/api/note/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": tokenValue,
+    },
+    body: JSON.stringify(note),
+  }).then((response) => response.json()).then((data) => {
+    return data;
+  });
+  return res;
 };
 
 export const addBucket = (bucket, completed) => {
-	var tokenValue = get(token);
+  var tokenValue = get(token);
 
-	var res = fetch(`${path}/api/bucketlist/bybucket`, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-			"Authorization": tokenValue,
-		},
-		body: JSON.stringify(bucket, completed),
-	}).then((response) => response.json()).then((data) => {
-		return data;
-	});
-	return res;
+  var res = fetch(`${path}/api/bucketlist/bybucket`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": tokenValue,
+    },
+    body: JSON.stringify(bucket, completed),
+  }).then((response) => response.json()).then((data) => {
+    return data;
+  });
+  return res;
 };
 export const addCard = (bucket) => {
-	var tokenValue = get(token);
+  var tokenValue = get(token);
 
-	var res = fetch(`${path}/api/flashcards/bycard`, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-			"Authorization": tokenValue,
-		},
-		body: JSON.stringify(bucket),
-	}).then((response) => response.json()).then((data) => {
-		return data;
-	});
-	return res;
+  var res = fetch(`${path}/api/flashcards/bycard`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": tokenValue,
+    },
+    body: JSON.stringify(bucket),
+  }).then((response) => response.json()).then((data) => {
+    return data;
+  });
+  return res;
 };
 
 export const getNote = (id) => {
-	var tokenValue = get(token);
+  var tokenValue = get(token);
 
-	var res = fetch(`${path}/api/note/?id=${id}`, {
-		method: "GET",
-		headers: {
-			"Content-Type": "application/json",
-			"Authorization": tokenValue,
-		},
-	}).then((response) => response.json()).then((data) => {
-		return data;
-	});
-	return res;
+  var res = fetch(`${path}/api/note/?id=${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": tokenValue,
+    },
+  }).then((response) => response.json()).then((data) => {
+    return data;
+  });
+  return res;
 };
 
 export const getBucket = (id) => {
-	var tokenValue = get(token);
+  var tokenValue = get(token);
 
-	var res = fetch(`${path}/api/bucketlist/bybucket/?id=${id}`, {
-		method: "GET",
-		headers: {
-			"Content-Type": "application/json",
-			"Authorization": tokenValue,
-		},
-	}).then((response) => response.json()).then((data) => {
-		return data;
-	});
-	return res;
+  var res = fetch(`${path}/api/bucketlist/bybucket/?id=${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": tokenValue,
+    },
+  }).then((response) => response.json()).then((data) => {
+    return data;
+  });
+  return res;
 };
 export const getCard = (id) => {
-	var tokenValue = get(token);
+  var tokenValue = get(token);
 
-	var res = fetch(`${path}/api/flashcards/bycard/?id=${id}`, {
-		method: "GET",
-		headers: {
-			"Content-Type": "application/json",
-			"Authorization": tokenValue,
-		},
-	}).then((response) => response.json()).then((data) => {
-		return data;
-	});
-	return res;
+  var res = fetch(`${path}/api/flashcards/bycard/?id=${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": tokenValue,
+    },
+  }).then((response) => response.json()).then((data) => {
+    return data;
+  });
+  return res;
 };
 export const updateNote = (note) => {
-	var tokenValue = get(token);
+  var tokenValue = get(token);
 
-	var res = fetch(`${path}/api/note/`, {
-		method: "PUT",
-		headers: {
-			"Content-Type": "application/json",
-			"Authorization": tokenValue,
-		},
-		body: JSON.stringify(note),
-	}).then((response) => response.json()).then((data) => {
-		return data;
-	});
-	return res;
+  var res = fetch(`${path}/api/note/`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": tokenValue,
+    },
+    body: JSON.stringify(note),
+  }).then((response) => response.json()).then((data) => {
+    return data;
+  });
+  return res;
 };
 
 export const updateBucket = (bucket) => {
-	var tokenValue = get(token);
+  var tokenValue = get(token);
 
-	var res = fetch(`${path}/api/flashcards/bycard`, {
-		method: "PUT",
-		headers: {
-			"Content-Type": "application/json",
-			"Authorization": tokenValue,
-		},
-		body: JSON.stringify(bucket),
-	}).then((response) => response.json()).then((data) => {
-		return data;
-	});
-	return res;
+  var res = fetch(`${path}/api/flashcards/bycard`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": tokenValue,
+    },
+    body: JSON.stringify(bucket),
+  }).then((response) => response.json()).then((data) => {
+    return data;
+  });
+  return res;
 };
 
 export const updateCard = (bucket) => {
-	var tokenValue = get(token);
+  var tokenValue = get(token);
 
-	var res = fetch(`${path}/api/flashcards/bycard`, {
-		method: "PUT",
-		headers: {
-			"Content-Type": "application/json",
-			"Authorization": tokenValue,
-		},
-		body: JSON.stringify(bucket),
-	}).then((response) => response.json()).then((data) => {
-		return data;
-	});
-	return res;
+  var res = fetch(`${path}/api/flashcards/bycard`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": tokenValue,
+    },
+    body: JSON.stringify(bucket),
+  }).then((response) => response.json()).then((data) => {
+    return data;
+  });
+  return res;
 };
